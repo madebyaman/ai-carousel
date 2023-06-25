a65eb706510c5641da94e3b2c299b48c04ce432c
Aman Thakur <builtbyaman@gmail.com>
2023-06-25 18:21:28 +0530

    commit

---
+++ components/Editor.tsx
@@ -0,0 +1,262 @@
+import React from 'react';
+import { Box, Flex } from '@chakra-ui/react';
+import jsPDF from 'jspdf';
+import { fabric } from 'fabric';
+
+import { FabricJSCanvas, useFabricJSEditor } from 'fabricjs-react';
+import { IoAdd } from 'react-icons/io5';
+
+const Editor = () => {
+  const { editor, onReady } = useFabricJSEditor();
+  const [editorState, setEditorState] = React.useState<{
+    activeIndex: number;
+    objects: (Object | null)[];
+  }>({
+    activeIndex: 0,
+    objects: [null],
+  });
+  const { activeIndex, objects } = editorState;
+
+  React.useEffect(() => {
+    if (editor) {
+      const handler = () => saveCanvasState(editor.canvas);
+      editor.canvas.on({
+        'object:modified': handler,
+        'object:added': handler,
+        'object:removed': handler,
+      });
+
+      return () => {
+        // clean up the event handlers when the component is unmounted or the editor changes
+        editor.canvas.off({
+          'object:modified': handler,
+          'object:added': handler,
+          'object:removed': handler,
+        });
+      };
+    }
+  }, [editor]);
+
+  const onAddCircle = () => {
+    editor?.addCircle();
+  };
+
+  const onAddText = () => {
+    const text = new fabric.IText('Hello, World!', { left: 10, top: 10 });
+    editor?.canvas.add(text);
+  };
+
+  const onAddRectangle = () => {
+    editor?.addRectangle();
+  };
+
+  const onAddImage = () => {
+    fabric.Image.fromURL('https://via.placeholder.com/150', function (oImg) {
+      oImg.scale(0.5);
+      editor?.canvas.add(oImg);
+    });
+  };
+
+  function saveCanvasState(canvas: any) {
+    const json = canvas?.toJSON();
+    let newObjects;
+
+    // Check if object already exists at the activeIndex
+    if (activeIndex < objects.length) {
+      // Update existing object
+      newObjects = objects.map((object, index) =>
+        index === activeIndex ? json : object
+      );
+    } else {
+      // Insert new object
+      newObjects = [
+        ...objects.slice(0, activeIndex),
+        json,
+        ...objects.slice(activeIndex),
+      ];
+    }
+
+    setEditorState({
+      ...editorState,
+      objects: newObjects,
+    });
+  }
+
+  async function exportPDF() {
+    const scaleFactor = 300 / 96; // Increase DPI from 96 (default) to 300
+    const sizeInMm = 1024 * 0.264583;
+
+    // Create a new jsPDF instance
+    const pdf = new jsPDF({
+      orientation: 'portrait',
+      unit: 'mm',
+      format: [sizeInMm, sizeInMm],
+    });
+
+    // Iterate over each JSON canvas state in the objects array
+    for (let i = 0; i < objects.length; i++) {
+      const json = objects[i];
+
+      // Create a new fabric Canvas instance
+      const tempCanvas = new fabric.Canvas(null);
+
+      // Await for the JSON data to be loaded into the canvas
+      await new Promise((resolve) => {
+        tempCanvas.loadFromJSON(json, resolve);
+      });
+
+      // Create a data URL of the canvas content, but with increased resolution
+      const imageData = tempCanvas.toDataURL({
+        format: 'png',
+        multiplier: scaleFactor,
+      });
+
+      // Add a new page if this isn't the first canvas
+      if (i > 0) {
+        pdf.addPage();
+      }
+
+      // Add the image to the PDF
+      const imgProps = pdf.getImageProperties(imageData);
+      const pdfWidth = pdf.internal.pageSize.getWidth();
+      const pdfHeight = (imgProps.height * pdfWidth) / imgProps.width;
+      pdf.addImage(imageData, 'PNG', 0, 0, pdfWidth, pdfHeight);
+    }
+
+    // Save the PDF
+    pdf.save('download.pdf');
+  }
+
+  React.useEffect(() => {
+    const json = objects[activeIndex];
+    loadJSON(json);
+  }, [activeIndex]);
+
+  const loadJSON = (JSON: object | null) => {
+    if (editor && editor.canvas) {
+      if (!JSON) {
+        // Clear the canvas if no SVG string is provided
+        editor.canvas.clear();
+      } else {
+        // Load the SVG string into the canvas
+        editor.canvas.loadFromJSON(JSON);
+      }
+    }
+  };
+
+  const handleImageUpload = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const file = event.target.files[0];
+    if (file) {
+      const url = URL.createObjectURL(file);
+      fabric.Image.fromURL(url, function (oImg) {
+        oImg.scale(0.5);
+        editor?.canvas.add(oImg);
+      });
+    }
+  };
+
+  const bringToFront = () => {
+    const activeObject = editor?.canvas.getActiveObject();
+    if (activeObject) {
+      activeObject.bringToFront();
+      editor?.canvas.renderAll();
+    }
+  };
+
+  const sendToBack = () => {
+    const activeObject = editor?.canvas.getActiveObject();
+    if (activeObject) {
+      activeObject.sendToBack();
+      editor?.canvas.renderAll();
+    }
+  };
+
+  React.useEffect(() => {
+    const handleClickOutsideCanvas = (event: any) => {
+      if (
+        editor &&
+        editor.canvas &&
+        !editor.canvas.wrapperEl.contains(event.target)
+      ) {
+        editor.canvas.discardActiveObject().renderAll();
+      }
+    };
+
+    document.addEventListener('mousedown', handleClickOutsideCanvas);
+
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutsideCanvas);
+    };
+  }, [editor]);
+
+  return (
+    <Box display={'flex'} flexDirection={'column'}>
+      {/* <button onClick={onAddCircle}>Add circle</button>
+      <button onClick={onAddRectangle}>Add Rectangle</button>
+      <button onClick={onAddText}>Add Text</button>
+      <button onClick={bringToFront}>Bring to Front</button>
+      <button onClick={sendToBack}>Send to Back</button>
+      <input type="file" onChange={handleImageUpload} />
+      {/* <button onClick={onAddImage}>Add Image</button> */}
+      {/* <button onClick={exportPDF}>Export</button>  */}
+      <Box
+        as={FabricJSCanvas}
+        width={'512px'}
+        height={'512px'}
+        placeSelf={'center'}
+        className="sample-canvas"
+        backgroundColor={'white'}
+        boxShadow={'md'}
+        borderRadius={'md'}
+        p="1"
+        onReady={onReady}
+      />
+      <Flex flexWrap={'wrap'} mt="4" gap="2" justifyContent={'center'}>
+        {objects.map((object, index) => (
+          <Box
+            as="button"
+            key={index}
+            border={'2px'}
+            borderColor={activeIndex === index ? 'blue.300' : 'gray.200'}
+            display={'grid'}
+            placeItems={'center'}
+            width="40px"
+            height="40px"
+            bgColor={'white'}
+            onClick={() => {
+              setEditorState({
+                activeIndex: index,
+                objects,
+              });
+            }}
+          >
+            {index + 1}
+          </Box>
+        ))}
+        <Box
+          as="button"
+          display={'grid'}
+          _hover={{
+            borderColor: 'gray.400',
+          }}
+          placeItems={'center'}
+          border={'2px'}
+          borderColor={'gray.200'}
+          bgColor={'white'}
+          width="40px"
+          height="40px"
+          onClick={() => {
+            setEditorState({
+              activeIndex: objects.length,
+              objects: [...objects, null],
+            });
+          }}
+        >
+          <IoAdd />
+        </Box>
+      </Flex>
+    </Box>
+  );
+};
+
+export default Editor;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,40 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import styles from '@/styles/Home.module.css'
+import Head from 'next/head';
+import Image from 'next/image';
+import { Inter } from 'next/font/google';
+import styles from '@/styles/Home.module.css';
+import Editor from '@/components/Editor';
+import { Box, Button, Flex, TabList, Tabs, Tooltip } from '@chakra-ui/react';
+import { IoImage, IoScanSharp, IoStar, IoText } from 'react-icons/io5';
+import React from 'react';
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ['latin'] });
+
+const tabs = [
+  {
+    name: 'Background',
+    icon: IoScanSharp,
+    children: <p>Background</p>,
+  },
+  {
+    name: 'Text',
+    icon: IoText,
+    children: <p>Text</p>,
+  },
+  {
+    name: 'Image',
+    icon: IoImage,
+    children: <p>Image</p>,
+  },
+  {
+    name: 'Shape',
+    icon: IoStar,
+    children: <p>Shape</p>,
+  },
+];
 
 export default function Home() {
+  const [tabIndex, setTabIndex] = React.useState(0);
+
   return (
     <>
       <Head>
@@ -14,101 +43,58 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={`${styles.main} ${inter.className}`}>
-        <div className={styles.description}>
-          <p>
-            Get started by editing&nbsp;
-            <code className={styles.code}>pages/index.tsx</code>
-          </p>
-          <div>
-            <a
-              href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              By{' '}
-              <Image
-                src="/vercel.svg"
-                alt="Vercel Logo"
-                className={styles.vercelLogo}
-                width={100}
-                height={24}
-                priority
-              />
-            </a>
-          </div>
-        </div>
+      <Box
+        as="main"
+        backgroundColor={'gray.50'}
+        display={'flex'}
+        flexDirection={'row'}
+        h="100vh"
+        w="100vw"
+      >
+        <Flex
+          borderRadius={'md'}
+          height={'100%'}
+          flexDir={'column'}
+          gap="5px"
+          py="5"
+          px="5px"
+          backgroundColor={'gray.100'}
+        >
+          {tabs.map((tab, index) => (
+            <Tooltip label={tab.name} key={tab.name} placement="right">
+              <Box
+                as="button"
+                p="3"
+                borderRadius={'md'}
+                bgColor={tabIndex === index ? 'white' : 'gray.100'}
+                boxShadow={tabIndex === index ? 'lg' : 'none'}
+                _hover={{
+                  bgColor: 'white',
+                }}
+                onClick={() => setTabIndex(index)}
+                display={'flex'}
+                gap="5px"
+              >
+                <tab.icon aria-describedby="name" />
+              </Box>
+            </Tooltip>
+          ))}
+        </Flex>
+        <Box
+          w={'25%'}
+          py="5"
+          px="10px"
+          maxW="sm"
+          borderRight={'1px'}
+          borderColor={'#eee'}
+        >
+          {tabIndex >= 0 ? tabs[tabIndex].children : null}
+        </Box>
 
-        <div className={styles.center}>
-          <Image
-            className={styles.logo}
-            src="/next.svg"
-            alt="Next.js Logo"
-            width={180}
-            height={37}
-            priority
-          />
-        </div>
-
-        <div className={styles.grid}>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2>
-              Docs <span>-&gt;</span>
-            </h2>
-            <p>
-              Find in-depth information about Next.js features and&nbsp;API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2>
-              Learn <span>-&gt;</span>
-            </h2>
-            <p>
-              Learn about Next.js in an interactive course with&nbsp;quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2>
-              Templates <span>-&gt;</span>
-            </h2>
-            <p>
-              Discover and deploy boilerplate example Next.js&nbsp;projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2>
-              Deploy <span>-&gt;</span>
-            </h2>
-            <p>
-              Instantly deploy your Next.js site to a shareable URL
-              with&nbsp;Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
+        <Flex py="5" grow="1" flexDir={'column'} height={'100%'}>
+          <Editor />
+        </Flex>
+      </Box>
     </>
-  )
+  );
 }
